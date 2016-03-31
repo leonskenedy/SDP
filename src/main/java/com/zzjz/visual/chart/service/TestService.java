81e53a0cfd60f413c52e3aca887d9f0f77f998c7
leon <leonskenedy03@gmail.com>
2016-03-31 10:37:06 +0800

    新增数据精确匹配功能

--- src/main/java/com/zzjz/visual/chart/service/TestService.java
+++ src/main/java/com/zzjz/visual/chart/service/TestService.java
@@ -18,17 +18,10 @@ public class TestService {
     @Autowired
     private SessionFactory factory;
 
-    public void save(Test test){
-        factory.openSession().save(test);
-    }
 
-    public void delete(Test test){
-        factory.openSession().delete(test);
-    }
 
-    public List<Test> getList(){
-        return factory.openSession().createQuery("from Test").list();
-    }
+
+
 
     public JSONArray getUniqueColumnData(String columnName, String keyWord){
         Session s = null;
